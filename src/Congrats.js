4c9545c3440a0aeba7430da840066f555b2931be
Vlad Kondratiev <vlad.kondratiev@ekreative.com>
2019-05-30 12:53:57 +0300

    congrats tests finished

---
+++ src/Congrats.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+/**
+ * Functional react component for congratulatory message
+ * @function
+ * @param {object} props
+ * @returns {JSX.Element} - rendered component or null if `success` prop is 
+ */
+const Congrats = props => {
+  return props.success ? (
+    <div data-test='component-congrats'>
+      <span data-test='congrats-message'>
+        Congratulations! You guessed the word!
+      </span>
+    </div>
+  ) : (
+    <div data-test='component-congrats' />
+  )
+}
+
+export default Congrats
---
+++ src/tests/utils.js
@@ -0,0 +1,9 @@
+/**
+ * return node(s) with the given data-test attribute
+ * @param {ShallowWrapper} wrapper
+ * @param {string} val
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
