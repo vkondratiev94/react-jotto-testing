73a59a6334d58c361fefd4d64bd98cc3786eedbc
Vlad Kondratiev <vlad.kondratiev@ekreative.com>
2019-05-30 16:55:51 +0300

    guessedWords tests and bootstrap styling finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+import Congrats from './Congrats'
+import GuessedWords from './GuessedWords'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Jotto</h1>
+      <Congrats success={true} />
+      <GuessedWords guessedWords={[
+        { guessedWord: 'traing', letterMatchCount: 3 }
+      ]}/>
     </div>
   );
 }
---
+++ src/GuessedWords.test.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { findByTestAttr, checkProps } from './tests/utils'
+import GuessedWords from './GuessedWords'
+
+const defaultProps = {
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
+}
+
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component
+ * @function setup
+ * @param {object} props
+ * @returns {ShallowWrapper} 
+ */
+const setup = (props = {}) => {
+  const setupProps = {...defaultProps, ...props}
+  return shallow(<GuessedWords {...setupProps} />)
+}
+
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps)
+})
+
+describe('if there are no words guesssed', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] })
+  })
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1)
+  })
+
+  test('renders instruction to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions')
+    expect(instructions.text().length).not.toBe(0)
+  })
+})
+
+describe('if there are words guessed', () => {
+  let wrapper
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 }
+  ]
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords })
+  })
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1)
+  })
+
+  test('renders guessed words section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words')
+    expect(guessedWordsNode.length).toBe(1)
+  })
+
+  test('displays correct number of guessed words', () => {
+    const guessedWordsNodes = findByTestAttr(wrapper, 'guessed-word')
+    expect(guessedWordsNodes.length).toBe(guessedWords.length)
+  })
+})
